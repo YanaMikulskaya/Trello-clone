4fc67f7ec3270d54dd4418a6218f654aee145872
YanaMikulskaya <miklushka26@gmail.com>
2026-01-20 17:14:36 +0300

    feat: add real-time clock to header

---
+++ src/js/createClock.js
@@ -0,0 +1,13 @@
+import { clockEl } from './dom';
+
+export function createClock() {
+    function updateClock() {
+        const now = new Date();
+        const hours = now.getHours().toString().padStart(2, '0');
+        const minutes = now.getMinutes().toString().padStart(2, '0');
+        clockEl.textContent = `${hours}:${minutes}`;
+    }
+    updateClock()
+    setInterval(updateClock, 60000);
+}
+
